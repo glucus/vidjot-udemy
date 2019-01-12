266d7b8134098324e55dec711cf85035ecb0c551
A.Voskresenskaya <A.Voskresenskaya@leomax.ru>
2019-01-13 00:10:26 +0300

    added index and about routes

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const callback = () => console.log(`Server started on port ${port}`);
+const listenCallback = () => console.log(`Server started on port ${port}`);
 
-app.listen(port, callback);
+// index route
+const indexCallback = (req, res) => res.send('Hello! Welcome to vidjot');
+app.get('/', indexCallback);
+
+// about route
+app.get ('/about', (req, res) => res.send('Vidjot is an app for your videos ideas'));
+
+app.listen(port, listenCallback);
