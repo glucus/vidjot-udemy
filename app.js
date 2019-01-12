4f5c74f7336ddb01bb4e82c8bfda181e90e3050c
glucus <voskresenskayaas@gmail.com>
2019-01-13 00:36:03 +0300

    example of middleware

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const port = 3000;
 
 const listenCallback = () => console.log(`Server started on port ${port}`);
 
+// middleware example
+app.use((req, res, next) => {
+    req.name = 'stranger';
+    next(); // to run the next middleware
+});
+
 // index route
-const indexCallback = (req, res) => res.send('Hello! Welcome to vidjot');
+const indexCallback = (req, res) => res.send(`Hello, ${req.name}! Welcome to vidjot`);
 app.get('/', indexCallback);
 
 // about route
-app.get ('/about', (req, res) => res.send('Vidjot is an app for your videos ideas'));
+app.get('/about', (req, res) => res.send('Vidjot is an app create to store your videos ideas'));
 
 app.listen(port, listenCallback);
